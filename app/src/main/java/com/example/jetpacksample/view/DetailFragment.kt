d245ca5915cee3254c29f143b43b61c599b2e1c3
Alireza Abiri <abiriali78@gmail.com>
2020-06-25 19:12:13 +0430

    Added navigation functionality

--- app/src/main/java/com/example/jetpacksample/view/DetailFragment.kt
+++ app/src/main/java/com/example/jetpacksample/view/DetailFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 
 import com.example.jetpacksample.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
@@ -16,6 +19,15 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        list_btn.setOnClickListener {
+            val action: NavDirections = DetailFragmentDirections.actionDetailFragmentToListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }
--- app/src/main/java/com/example/jetpacksample/view/ListFragment.kt
+++ app/src/main/java/com/example/jetpacksample/view/ListFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 
 import com.example.jetpacksample.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -16,6 +19,15 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        details_btn.setOnClickListener {
+            val action: NavDirections = ListFragmentDirections.actionListFragmentToDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 }
