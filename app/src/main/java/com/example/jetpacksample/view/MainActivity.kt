789371dac2b5fe8171410ab8a341ac096d0ffa7b
Alireza Abiri <abiriali78@gmail.com>
2020-06-25 19:06:02 +0430

    Fixed layout being out of display usable height

--- app/src/main/java/com/example/jetpacksample/view/MainActivity.kt
+++ app/src/main/java/com/example/jetpacksample/view/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.jetpacksample.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
+import android.util.DisplayMetrics
+import androidx.appcompat.app.AppCompatActivity
 import com.example.jetpacksample.R
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        getSoftButtonsBarSizePort(this)
     }
+
+//    fun getSoftButtonsBarSizePort(activity: Activity): Int { // getRealMetrics is only available with API 17 and +
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+//            val metrics = DisplayMetrics()
+//            activity.windowManager.defaultDisplay.getMetrics(metrics)
+//            val usableHeight = metrics.heightPixels
+//            activity.windowManager.defaultDisplay.getRealMetrics(metrics)
+//            val realHeight = metrics.heightPixels
+//            return if (realHeight > usableHeight) realHeight - usableHeight else 0
+//        }
+//        return 0
+//    }
 }
